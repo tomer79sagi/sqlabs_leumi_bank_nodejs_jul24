9cfcf9684c4f762db6744e15c27a1394841fe604
tomer79sagi <me@tomersagi.com>
2024-07-15 18:28:34 +0300

    Lesson updates

--- Lesson 2 - 15.7.24/chatServer/chatServer.js
+++ Lesson 2 - 15.7.24/chatServer/chatServer.js
@@ -1,9 +1,9 @@
-// 1. Import 'http' module
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const WebSocket = require('ws');
 
-// 2. Create a server
+// -- 1. CREATE A HTTP SERVER --
 const server = http.createServer((request, response) => {
     // 3.1 Parse URL and determine filename
     // 3.2 If no 'path' is defined, return 'index.html'
@@ -56,7 +56,30 @@ const server = http.createServer((request, response) => {
     });
 });
 
-// 4. Start the server
+
+// -- 2. INITIALIZE THE WS SERVER --
+const wss = new WebSocket.Server({ server });
+
+// Handling Client Connections
+wss.on('connection', ws => {
+    // A) In case of a message from a client
+    ws.on('message', message => {
+        console.log(`Received: ${message}`);
+
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    // B) Send a 'connection' message
+    console.log('Client connected');
+    ws.send('Welcome the chat');
+});
+
+
+// -- 3. START THE SERVER --
 const PORT = 3006;
 server.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
