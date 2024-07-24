c02d122fa3ee2bfaced04e01f104b8b8e9e588a5
tomer79sagi <me@tomersagi.com>
2024-07-24 13:44:40 +0300

    Added GET transactions

--- Lesson 5 - 24.7.24/express_ejs_bank/app.js
+++ Lesson 5 - 24.7.24/express_ejs_bank/app.js
@@ -3,9 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const resLogger = require('./middlewares/response-logger');
 
-// -- DB IMPORTS --
+// -- DB & CONFIG IMPORTS --
 const mongoose = require('mongoose');
 const Transaction = require('./models/transaction');
+const dotenv = require('dotenv');
 
 const app = express();
 const port = 3000;
@@ -25,7 +26,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(resLogger);
 
 
-// -- DATA / DB --
+// -- DATA / DB / CONFIG --
+dotenv.config();
+
 const user = {
     name: 'John Doe',
     accounts: [
@@ -36,9 +39,9 @@ const user = {
 
 // -- USE MONGODB --
 // 1. Create connection string
-const connectionString = 'mongodb+srv://tomer79sagi:uW0JaI6kRZfd0Ruw@cluster0.q4m7kxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const connectionString = process.env.MONGODB_URI || 'mongodb+srv://tomer79sagi:uW0JaI6kRZfd0Ruw@cluster0.q4m7kxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 // 2. Connect to MongoDB
-mongoose.connect(connectionString, { useNewUrlParser: true})
+mongoose.connect(connectionString)
     .then(() => {
         console.log("MongoDB Connected Successfully!");
     })
@@ -59,7 +62,7 @@ app.get('/transfer', (req, res) => {
     res.render('transfer', { user: user });
 });
 
-app.post('/transfer', (req, res) => {
+app.post('/transfer', async (req, res) => {
     const fromAccount = user.accounts.find(account => account.number === req.body.fromAccount);
     const toAccount = user.accounts.find(account => account.number === req.body.toAccount);
     const amount = parseFloat(req.body.amount);
@@ -68,18 +71,34 @@ app.post('/transfer', (req, res) => {
         fromAccount.balance -= amount;
         toAccount.balance += amount;
 
-        transactions.push({
-            date: new Date().toLocaleString(),
-            amount: amount,
-            from: fromAccount.number,
-            to: toAccount.number
-        });
-    }
+        // -- NEW, using MongoDB --
+        try {
+            const transaction = new Transaction({
+                amount: amount,
+                fromAccount: fromAccount.number,
+                toAccount: toAccount.number
+            });
 
-    res.redirect('/');
+            await transaction.save();
+            res.redirect('/');
+        } catch (err) {
+            console.log(`DB Error: ${err.message}`);
+            res.redirect('/');   
+        }
+
+        // -- OLD, using Array --
+    //     transactions.push({
+    //         date: new Date().toLocaleString(),
+    //         amount: amount,
+    //         from: fromAccount.number,
+    //         to: toAccount.number
+        // });
+    }
 });
 
-app.get('/transactions', (req, res) => {
+app.get('/transactions', async (req, res) => {
+    const transactions = await Transaction.find();
+
     res.render('transactions', { transactions: transactions });
 });
 
